79547bc7d011d4a0f5bdb38c59d6aa5e2596cd3b
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-26 22:17:57 +0530

    install dependency-ReactRouterDom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 
-function App() {
+import './App.css';
+import Navbar from './components/Navbar';
+
+const App = () => {
   return (
-    <div className="App">
-      Welcome to Coin Verse
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
-  );
-}
+  )
+};
 
 export default App;
