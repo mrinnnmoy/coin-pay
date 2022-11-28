5b950cb9a9503ebf3505929b84e048eb3fb31e18
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-28 10:44:50 +0530

    build api-dev

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Note: Change v1 to v2 on rapid api
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+  "X-RapidAPI-Key": "eeda8a679dmshb749c246088e800p18debcjsn3496385f44ed",
+};
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/exchanges")
+    })
+  })
+});
