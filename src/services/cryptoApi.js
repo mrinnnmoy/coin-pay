507335343429192433cc5781f36ed8dd19f11232
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-29 09:54:24 +0530

    fix components

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -3,20 +3,18 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Note: Change v1 to v2 on rapid api
 
 const cryptoApiHeaders = {
-  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
-  "X-RapidAPI-Key": "eeda8a679dmshb749c246088e800p18debcjsn3496385f44ed",
+  "X-RapidAPI-Host": process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
+  "X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_KEY,
 };
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
-const baseUrl = "https://coinranking1.p.rapidapi.com";
-
 export const cryptoApi = createApi({
-  reducerPath: "cryptoApi",
-  baseQuery: fetchBaseQuery({ baseUrl }),
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: (count) => createRequest("/coins")
+      query: (count) => createRequest("/coins"),
     }),
   }),
 });
